9a2d855e9a7376e0ae8a1636d11d2aad33c3b9aa
Yigit Guler <yigit@hipo.biz>
2015-05-29 19:03:10 +0300

    change order

--- light_controller/settings.py
+++ light_controller/settings.py
@@ -4,8 +4,8 @@ DATA_STORAGE_PATH = "data.json"
 
 
 LAMPS = {
-    "relay-0": 2,
-    "relay-1": 3,
-    "relay-2": 14,
-    "relay-3": 15,
+    "relay-0": 15,
+    "relay-1": 14,
+    "relay-2": 3,
+    "relay-3": 2,
 }
